e083c0c9a125c7b282fdf729c3c7b0465ebcdc84
ValKop <valentinkopanev@yandex.ru>
2020-01-27 22:55:59 +0300

    new function and fix bugs

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from . models import Post
 from django.utils import timezone
 from django.contrib.auth.models import User
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')
@@ -13,3 +15,17 @@ def post_detail(request, pk):
                                                      'first_name':User.objects.get(username=post.author).first_name,
                                                      'last_name':User.objects.get(username=post.author).last_name,
                                                      })
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.created_date = timezone.now()
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form':form})
