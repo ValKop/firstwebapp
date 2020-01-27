dc27697ddf60717d63cff611f9ab00d95e957d3a
ValKop <valentinkopanev@yandex.ru>
2020-01-27 17:35:53 +0300

    Fix AuthorName

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from . models import Post
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
@@ -8,4 +9,4 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post':post})
+    return render(request, 'blog/post_detail.html', {'post':post, 'first_name':User.objects.get(username=post.author).first_name, 'last_name':User.objects.get(username=post.author).last_name})
