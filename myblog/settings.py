e249f9b919d5af126fc1ddd9f07fe131dece2bc4
ValKop <valentinkopanev@yandex.ru>
2020-01-27 22:58:43 +0300

    dubug set false

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#s-gg9dm6bj1vg$m9i+76^$y+#iz=%sf!pbbmeec4x)*_4w9o&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
